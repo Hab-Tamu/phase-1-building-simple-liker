db421c6e72ad95a1c4f830468df407f112e85e22
Habtamu Debele <habtlig2010@gmail.com>
2022-05-17 20:17:23 -0400

    First draft complete

--- main.js
+++ main.js
@@ -4,7 +4,38 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const unlikedHeart = document.querySelectorAll('span')
 
+unlikedHeart.forEach(element => {
+  
+  element.addEventListener('click', ()=>{
+    
+    likedHeart()
+    mimicServerCall().catch(function(error){
+      
+      document.getElementById('modal').className = "visible"
+      likedHeart()
+      
+      setTimeout(function(){ 
+        document.getElementById('modal').className = "hidden"
+      }, 3000);
+     
+    })
+   
+  });
+  
+  function likedHeart(){
+    if (element.className === "like-glyph"){
+      element.innerHTML = FULL_HEART
+      element.className = "like-glyph activated-heart"
+  
+    } else if (element.className === "like-glyph activated-heart") {
+      element.innerHTML = EMPTY_HEART
+      element.className = "like-glyph"
+    }
+}
+
+})
 
 
 //------------------------------------------------------------------------------
